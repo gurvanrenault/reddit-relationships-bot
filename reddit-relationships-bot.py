ff73ffefabcba3709f95fb1f9bc756467dea6544
Gurvan Renault <renault.gurvan@gmail.com>
2017-07-03 03:34:29 +0200

    Add an analyse of OP gender

--- reddit-relationships-bot.py
+++ reddit-relationships-bot.py
@@ -1,7 +1,7 @@
 import os
 import praw
 from settings import SETTINGS
-
+from  social_analysis import *  
 reddit = praw.Reddit(client_id=SETTINGS['REDDIT_CLIENT_ID'],
                      client_secret=SETTINGS['REDDIT_CLIENT_SECRET'],
                      user_agent=SETTINGS['REDDIT_USER_AGENT'])
@@ -84,6 +84,9 @@ def display_information():
     for cliche in cliches:
         print "%s: %s" % (cliche['advice'], len(cliche['comments']))
 
+def analyse_OP_age(submission):
+    print "Anal"
+    sip
 
 def analyse_comment(comment):
     for cliche in cliches:
@@ -95,13 +98,17 @@ def analyse_comment(comment):
 submissions = subreddit.hot(limit=SETTINGS["SUBMISSIONS_TO_ANALYSE"])
 submissions_analysed = 0
 comments_analysed = 0
-
+man=0
+women=0
+gender_analysed=0;
+print submissions
 for submission in submissions:
     for comment in submission.comments.list():
         analyse_comment(comment)
         comments_analysed += 1
-        display_information()
-
+        display_information();
+    man,women,gender_analysed=analyse_gender_OP(submission.title,man,women,gender_analysed)
+    display_analyse_gender_OP(man,women,gender_analysed);
     submissions_analysed += 1
 
 print "\nComplete."
---
+++ social_analysis.py
@@ -0,0 +1,38 @@
+# This file contain all the functions for social analysis  ( gender , age , subject  ... ) posted in /r/relationships/
+
+def is_OP(div_id,sliped_post):
+    # Check if the formatted string is about the Original Poster
+    op_marker=["me","i","myself","my"];
+    if (sliped_post[div_id-1].lower() in op_marker):
+        return True;
+    return False;
+
+def analyse_gender_OP(post,man,women,tot_gender):
+    # Analyse the gender of the Original Poster
+    gender=False;
+    open_brackets=["[","("];
+    end_brackets=["]",")"];
+  
+    post_splitted=post.split(" ")
+    for div_id in range(0,len(post_splitted)-1):
+    
+        if ((post_splitted[div_id][0] in open_brackets)and (post_splitted[div_id][len(post_splitted[div_id])-1]in end_brackets)and is_OP(div_id,post_splitted) ):
+           #  If the format look like [42F] or (42F)  
+            if (post_splitted[div_id].count("f")!=0 or  post_splitted[div_id].count("F")!=0):
+                # OP is a women
+                women=women+1;
+                tot_gender=tot_gender+1;
+            if (post_splitted[div_id].count("m")!=0 or  post_splitted[div_id].count("M")!=0):
+                #OP is a men
+                man=man+1
+                tot_gender=tot_gender+1;
+    return ( man,women,tot_gender);
+
+def  display_analyse_gender_OP(man,women,total):
+    print " Analysing the gender of originals posters .."
+    print "%s: %s" % ("Percentage of OP  men ",( (man*1.0)/total)*100) +'%'
+    print "%s: %s " % ("Percentage of OP  women " , ( (women*1.0)/total)*100) +'%'
+
+
+               
+             
